8bad55688327708a0e7d6400d93744d08a590236
Christian Weyer <christian.weyer@thinktecture.com>
2014-04-15 12:42:14 +0200

    Lunch

---
+++ MyTunes.WebHost/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace MyTunes.WebHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            config.Routes.MapHttpRoute("DefaultWebApi",
+                "{controller}");
+        }
+    }
+}
---
+++ MyTunes.WebHost/TracksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using MyTunes.BusinessLogic;
+using MyTunes.BusinessLogic.Interfaces;
+
+namespace MyTunes.WebHost
+{
+    public class TracksController : ApiController
+    {
+        private ITracksManager tracksManager;
+
+        public TracksController()
+        {
+            tracksManager = new TracksManager();
+        }
+
+        // List tracks
+        public List<TrackDto> Get()
+        {
+            return tracksManager.ListTracks();
+        }
+
+        // Get track details
+        public TrackDetailsDto Get(int id)
+        {
+            return tracksManager.GetTrackDetails(id);
+        }
+    }
+}
