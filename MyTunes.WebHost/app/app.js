a1afca2a0d1b0fa98393861b77b875a50fe14dd4
Christian Weyer <christian.weyer@thinktecture.com>
2014-04-16 15:42:27 +0200

    After AngularJS client

---
+++ MyTunes.WebHost/app/app.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("myTunes", ["ngRoute", "angular-loading-bar"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/tracks", {
+            templateUrl: "tracks/tracksList.html",
+            controller: "tracksController"
+        })
+        .when("/trackdetails/:id", {
+            templateUrl: "tracks/trackDetails.html",
+            controller: "trackDetailsController"
+        })
+        .when("/about", {
+            templateUrl: "about/about.html"
+        })
+        .otherwise({ redirectTo: "/tracks" });
+});
---
+++ MyTunes.WebHost/app/tracks/tracksController.js
@@ -0,0 +1,12 @@
+﻿app.controller("tracksController", function ($scope, $http, $location) {
+
+    $http.get("../tracks").then(function(result) {
+        $scope.tracks = result.data;
+    }, function(error) {
+        alert(error);
+    });
+
+    $scope.selectTrack = function (track) {
+        $location.path("/trackdetails/" + track.Id);
+    };
+});
