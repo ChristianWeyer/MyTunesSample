05638a8dcceeb6a513936e2aa8b59367e64a81a4
Christian Weyer <christian.weyer@thinktecture.com>
2014-04-16 08:39:52 +0200

    Everything included for the attendees

---
+++ MyTunes.BusinessLogic/DataMapper.cs
@@ -0,0 +1,31 @@
+﻿using MyTunes.BusinessLogic.Interfaces;
+using MyTunes.DataAccess;
+
+namespace MyTunes.BusinessLogic
+{
+    public static class DataMapper
+    {
+        public static TrackDetailsDto ToDetailsDto(this Track track)
+        {
+            return new TrackDetailsDto
+            {
+                Id = track.TrackId,
+                Name = track.Name,
+                Composer = track.Composer,
+                Duration = track.Milliseconds,
+                Size = track.Bytes
+            };
+        }
+
+        public static Track ToTrack(this TrackDetailsDto trackDetailsDto)
+        {
+            return new Track
+            {
+                Name = trackDetailsDto.Name,
+                Bytes = trackDetailsDto.Size,
+                Milliseconds = trackDetailsDto.Duration,
+                Composer = trackDetailsDto.Composer
+            };
+        }
+    }
+}
---
+++ MyTunes.BusinessLogic/TracksManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using MyTunes.BusinessLogic.Interfaces;
+using MyTunes.DataAccess;
+
+namespace MyTunes.BusinessLogic
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TracksManager : ITracksManager
+    {
+        public List<TrackDto> ListTracks()
+        {
+            using (var db = new MyTunesDatabaseContext())
+            {
+                // TODO: Remove hard-wired "paging"
+                var tracks = db.Tracks.Take(20).Select(
+                    t => new TrackDto
+                    {
+                        Id = t.TrackId,
+                        Name = t.Name
+                    }).ToList();
+
+                return tracks;
+            }
+        }
+
+        public TrackDetailsDto GetTrackDetails(int id)
+        {
+            using (var db = new MyTunesDatabaseContext())
+            {
+                var track = db.Tracks
+                    .FirstOrDefault(t => t.TrackId == id).ToDetailsDto();
+
+                return track;
+            }
+        }
+
+        public TrackDetailsDto AddTrack(TrackDetailsDto trackDetailsDto)
+        {
+            using (var db = new MyTunesDatabaseContext())
+            {
+                var entity = trackDetailsDto.ToTrack();
+                entity.MediaTypeId = 1;
+
+                db.Entry(entity).State = EntityState.Added;
+                db.SaveChanges();
+
+                return entity.ToDetailsDto();
+            }
+        }
+
+        public void DeleteTrack(int trackId)
+        {
+            using (var db = new MyTunesDatabaseContext())
+            {
+                var entity = new Track { TrackId = trackId };
+                
+                db.Entry(entity).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+    }
+}
